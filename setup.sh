39f0b48d368d2a74baa9ae6ed8ceeb2a32e836f9
jur-ch1k <zelenchukgeorgiy@mail.ru>
2026-07-12 23:43:44 +0300

    update setup again

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ echo "=== Шаг 4/5: flash-linear-attention (без зависимостей) =
 pip install flash-linear-attention[cuda] \
     --no-build-isolation \
     --no-cache-dir \
-    --no-deps \
     --no-binary flash-linear-attention \
     --force-reinstall
 
